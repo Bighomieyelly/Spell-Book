78274f0c7a59bcbf7299e0ab85b795b1e17f85c6
Bighomieyelly <33910041+Bighomieyelly@users.noreply.github.com>
2018-11-01 09:37:41 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".harry").click(function() {
+    $(".control1").fadeToggle();
+});
+
+$(".spell").click(function() {
+    $(".control2").slideToggle();
+});
+
+$(".show").click(function() {
+    $(".control3").toggle();
+});
